c0e1ed15b0b7b29cecc3e2660beadab758ab20a1
Mohammad Ali Sorush <m.alisorush8080@gmail.com>
2026-02-01 10:42:48 +0430

    POST Mehtod used in axios

--- axios.js
+++ axios.js
@@ -1,20 +1,37 @@
 const axios=require('axios');
 
 
-axios.get("https://jsonplaceholder.typicode.com/users")
-.then(res=>{
-    let id=1;
-    res.data.forEach(user=>{
-        // console.log(id+" : "+user.name);
-        // console.log("Username : "+user.username);
-        // console.log("Street : "+user.address.street);
-        console.log("  Name   , street   , city");
-        console.log(`${user.name} → ${user.address.street}, ${user.address.city}`);
-        id++;
+// axios.get("https://jsonplaceholder.typicode.com/users")
+// .then(res=>{
+//     let id=1;
+//     res.data.forEach(user=>{
+//         console.log("  Name   , street   , city");
+//         console.log(`${user.name} → ${user.address.street}, ${user.address.city}`);
+//         id++;
         
-    })
+//     })
    
-    // console.log(res.data);
-}).catch(err=>{
-    console.log(err.message);
-})
+//     // console.log(res.data);
+// }).catch(err=>{
+//     console.log(err.message);
+// })
+
+
+async function createUser() {
+  try {
+    const response = await axios.post(
+      "https://jsonplaceholder.typicode.com/posts",
+      {
+        title: "Hello",
+        body: "This is Axios",
+        userId: 1
+      }
+    );
+
+    console.log(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+createUser();
