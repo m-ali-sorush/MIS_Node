224ecbd320b734852ac4d344ef51ea938398da4a
Mohammad Ali Sorush <m.alisorush8080@gmail.com>
2026-02-01 10:39:56 +0430

    GET Method used in axios

--- axios.js
+++ axios.js
@@ -5,9 +5,15 @@ axios.get("https://jsonplaceholder.typicode.com/users")
 .then(res=>{
     let id=1;
     res.data.forEach(user=>{
-        console.log(id+" : "+user.name);
+        // console.log(id+" : "+user.name);
+        // console.log("Username : "+user.username);
+        // console.log("Street : "+user.address.street);
+        console.log("  Name   , street   , city");
+        console.log(`${user.name} → ${user.address.street}, ${user.address.city}`);
         id++;
+        
     })
+   
     // console.log(res.data);
 }).catch(err=>{
     console.log(err.message);
